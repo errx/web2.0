bf21c9034c5667e53b61338294eafdd728772493
Oleg Matrokhin <oyumatrokhin@avito.ru>
2018-02-15 11:23:54 +0300

    fix typo

--- src/Components/TriggerInfo/TriggerInfo.js
+++ src/Components/TriggerInfo/TriggerInfo.js
@@ -132,7 +132,7 @@ export default function TriggerInfo({
                             Please <RouterLink to={`/trigger/${data.id}/edit`}>verify</RouterLink> trigger target{hasMultipleTargets
                                 ? "s"
                                 : ""}
-                            {hasExpression ? " and exression" : ""} on{" "}
+                            {hasExpression ? " and expression" : ""} on{" "}
                             <RouterLink to={`/trigger/${data.id}/edit`}>trigger edit page</RouterLink>.
                             {supportEmail != null && (
                                 <span>
